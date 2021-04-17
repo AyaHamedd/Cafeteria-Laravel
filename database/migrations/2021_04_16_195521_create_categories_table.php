df30d8ec2903da6668121bb99621746a38ce83b3
ean <emyrade29@gmail.com>
2021-04-16 20:18:51 -0400

    implement category migrations

--- database/migrations/2021_04_16_195521_create_categories_table.php
+++ database/migrations/2021_04_16_195521_create_categories_table.php
@@ -15,7 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('label');
             $table->timestamps();
+           
         });
     }
 
