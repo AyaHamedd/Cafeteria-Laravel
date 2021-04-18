bee604d4aad662f6274248a481b445a0284e7ca6
Aya <ayahamedabdulaziz@gmail.com>
2021-04-18 23:21:25 +0200

    feature(products): Implemented Api to get products list

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource("/products",ProductController::class);
+
+Route::get("/testapi",function(){ return"I am called";});
