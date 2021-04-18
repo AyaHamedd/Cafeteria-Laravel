13779f9a5e9052015638629c5a9febcb18283345
AhmedAtef3 <ahmaadatef3@gmail.com>
2021-04-18 16:50:05 +0200

    feature(auth-registration): change template for vue

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,16 +11,16 @@ class RegisterController extends Controller
 {
     public function register(Request $request){
         $request->validate([
-            'name' => ['required'],
+            'name' => ['required'], 
             'email' => ['required', 'email', 'unique:users'],
             'password'=>['required', 'min:8', 'confirmed']
         ]);
 
-        $user = User::create(
-            $request->only('name', 'email')
-            +['password'=>Hash::make($request->input('password'))]
-        );
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=>Hash::make($request->password)
+        ]);
 
-        return response($user, RESPONSE::HTTP_CREATED);
     }
 }
