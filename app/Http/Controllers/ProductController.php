1f5d8e159f9a236fba85cf83df1615b83b778709
Aya <ayahamedabdulaziz@gmail.com>
2021-04-20 02:08:27 +0200

    feature(get-products): paginate fetched products.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products=Product::all();
+        $products=Product::where('is_available', 1)->paginate();
         return ProductResource::collection($products);
     }
 
