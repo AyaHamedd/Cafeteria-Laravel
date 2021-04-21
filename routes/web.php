883afacee3f98f54d1cb960e5225e3f02db2ffa6
ean <emyrade29@gmail.com>
2021-04-21 19:18:26 -0400

    adminPage-user-Crud-upload Image

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ImageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::resource('pages', 'AdminController');
+// // Route::resource('pages', 'AdminController');
+// Route::post('upload', App\Http\Controllers\ImageController::class);
