89e1a22721ca8980e7ac94f9e66b094758e7f388
ean <emyrade29@gmail.com>
2021-04-21 01:29:48 -0400

    set supports_credentials with true and this handles the error in server :)

--- config/cors.php
+++ config/cors.php
@@ -29,6 +29,15 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
+
+    // 'paths' => ['api/*'],
+    // 'allowed_methods' => ['*'],
+    // 'allowed_origins' => ['*'],
+    // 'allowed_origins_patterns' => [],
+    // 'allowed_headers' => ['*'],
+    // 'exposed_headers' => [],
+    // 'max_age' => 0,
+    // 'supports_credentials' => true,
 
 ];
