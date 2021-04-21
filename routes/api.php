7f2539ef829b38de607e92cb1de827ac1a9ad4f8
ean <emyrade29@gmail.com>
2021-04-21 01:32:00 -0400

    api.php is file modified  and api routers for AdminController and RoomController are added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Route::Resource('pages', 'App\Http\Controllers\AdminController');
+Route::resource('pages', App\Http\Controllers\AdminController::class); 
+ Route::resource('rooms', App\Http\Controllers\RoomController::class); 
+ Route::post('/upload', [App\Http\Controllers\ImageController::class, 'upload'])->name('upload');
